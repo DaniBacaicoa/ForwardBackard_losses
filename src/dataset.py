c8c78f373e3b34fe8bec5b8c12e5001eccdb6e92
danibacaicoa <dbacaico@ing.uc3m.es>
2025-01-25 21:39:32 +0100

    break added

--- src/dataset.py
+++ src/dataset.py
@@ -218,8 +218,9 @@ class Data_handling(Dataset):
                 fine_label_names[i]: coarse_label_names[fine_to_coarse_mapping[i]]
                 for i in range(len(fine_label_names))
             }
+
             #train_fine_labels_one_hot = self.train_dataset.targets
-            #breakpoint()
+            breakpoint()
             #train_fine_labels_one_hot = torch.tensor(train_fine_labels_one_hot)
             #train_fine_label_indices = torch.argmax(train_fine_labels_one_hot, dim=1)
             self.train_dataset.targets = torch.tensor([fine_to_coarse_mapping[fine_idx] for fine_idx in self.train_dataset.targets])
